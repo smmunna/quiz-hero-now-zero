54737a6d702c696fa5e7c918d4110851a8a8e507
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-15 22:32:44 +0600

    id="quizContainer added

--- index.js
+++ index.js
@@ -61,21 +61,22 @@ const displayQuiz = (data) => {
   }
 
   data.forEach((quiz,i) => {
-    console.log(quiz);
-    quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
-  <div class="flex items-center">
-    <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
-      ${i+1}
-    </div>
-    <p class="text-gray-800 text-sm">${quiz.question}</p>
-  </div>
-  <div class="grid grid-cols-2 gap-4 mt-5">
-  ${displayQuizOptions(quiz.options,i)}
-  </div>
-</div>`;
+    console.log(i+1);
+    console.log(quizContainer)
+    quizContainer.innerHTML += `
+    <div class="m-3 py-3 px-4 shadow-sm rounded">
+      <div class="flex items-center">
+        <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
+          ${i+1}
+        </div>
+       <p class="text-gray-800 text-sm">${quiz.question}</p>
+      </div>
+        <div class="grid grid-cols-2 gap-4 mt-5">
+           ${displayQuizOptions(quiz.options,i)}
+        </div>
+    </div>`;
   });
 };
-console.log(quizContainer);
 
 // EventListener for quiz submit button
 document.querySelector("#submit").addEventlistener("click", () => {
