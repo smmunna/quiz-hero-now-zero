12357000dbaa0ef513ad91029a38876192f12e81
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-15 22:02:19 +0600

    res.json update to res.json()

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
   displayQuiz(data);
 };
